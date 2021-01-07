130ad1a9bc606ced6240359fcbb7e2d5d6f11083
VÖRÖSKŐI András <voroskoi@gmail.com>
2021-01-07 21:42:43 +0100

    Add cli version

--- cmd/cyoa/main.go
+++ cmd/cyoa/main.go
@@ -2,22 +2,48 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 
-	"github.com/voroskoi/cyoa/cyoa"
+	"github.com/voroskoi/cyoa"
+)
+
+var (
+	story = flag.String("story", "../../gopher.json", "JSON file that contains the story")
+	web   = flag.Bool("web", false, "Run web werson, not the CMD based.")
 )
 
 func main() {
-	story := flag.String("story", "../../gopher.json", "JSON file that contains the story")
 	flag.Parse()
 
 	ps := cyoa.ParseJSON(*story)
 
-	cyoahandler := cyoa.NewHandler(ps)
+	if *web {
+		cyoahandler := cyoa.NewHandler(ps)
 
-	err := http.ListenAndServe(":8080", cyoahandler)
-	if err != nil {
-		log.Fatalf("server stopped inexpectedly: %s", err)
+		err := http.ListenAndServe(":8080", cyoahandler)
+		if err != nil {
+			log.Fatalf("server stopped inexpectedly: %s", err)
+		}
+	} else {
+		printStory(ps, "intro")
 	}
+}
+
+func printStory(ps cyoa.Story, chapter string) {
+	ch := ps[chapter]
+	fmt.Printf("Title: %s\n\n", ch.Title)
+	for _, p := range ch.Story {
+		fmt.Println(p)
+	}
+	for i := range ch.Options {
+		fmt.Printf("\nPress %d to continue with: %s\n", i, ch.Options[i].Text)
+	}
+	if len(ch.Options) == 0 {
+		return
+	}
+	var jump int
+	fmt.Scanln(&jump)
+	printStory(ps, ch.Options[jump].Arc)
 }
